5ed93cc54c28b63c8eddfcad9041ec673768f94d
Philippe-Nomena <philippenomena@gmail.com>
2026-04-16 23:00:56 +0300

    fix preview pour render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,22 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // 🔧 DEV (local)
   server: {
     port: 5173,
+    allowedHosts: ["crudai-front.onrender.com"],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
-  },
-
-  // 🚀 PROD (Render)
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 10000,
-    allowedHosts: "all",
   },
 });
